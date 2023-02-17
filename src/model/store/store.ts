33b1fff955bbedee7f7c099ab291edc7d943f37b
Juan Esteban Villegas Arias <estebanari29@gmail.com>
2023-02-17 16:07:02 -0500

    feat: deleted inventory context

--- src/model/store/store.ts
+++ src/model/store/store.ts
@@ -2,14 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../../controller/slices/user';
 import cocktailsReducer from '../../controller/slices/cocktails';
 import ordersReducer from '../../controller/slices/orders';
-import inventoryReducer from '../../controller/slices/inventory';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     cocktails: cocktailsReducer,
     orders: ordersReducer,
-    inventory: inventoryReducer,
   },
 });
 
